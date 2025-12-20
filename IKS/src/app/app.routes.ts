56c3257a36a3abdb1a5602c1e50833e57547ce19
Lenka Nikolic <lenkanikolic17@gmail.com>
2025-12-20 11:03:05 +0100

    added new component forgot-password and rearranged reset-password;

--- IKS/src/app/app.routes.ts
+++ IKS/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: 'login', loadComponent: () => import('./forms/login/login').then(m => m.Login) },
   { path: 'register', loadComponent: () => import('./forms/register/register').then(m => m.Register) },
+  { path: 'forgot-password', loadComponent: () => import('./forms/forgot-password/forgot-password').then(m => m.ForgotPassword) },
   { path: 'reset-password', loadComponent: () => import('./forms/reset-password/reset-password').then(m => m.ResetPassword) },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
