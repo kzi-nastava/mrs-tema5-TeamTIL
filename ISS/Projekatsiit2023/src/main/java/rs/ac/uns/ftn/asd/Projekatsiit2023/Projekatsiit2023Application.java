0a322e2fa3d6b522dfec85f938644f6223c67fec
Lenka Nikolic <lenkanikolic17@gmail.com>
2025-12-25 18:33:41 +0100

    add initial setup for backend part of the project

---
+++ ISS/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/Projekatsiit2023Application.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekatsiit2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekatsiit2023Application.class, args);
+	}
+
+}
