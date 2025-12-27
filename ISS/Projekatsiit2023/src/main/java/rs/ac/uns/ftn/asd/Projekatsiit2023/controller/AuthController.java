7e1f5e56d16e4a1d6908c9dacf99c56f53536941
Lenka Nikolic <lenkanikolic17@gmail.com>
2025-12-27 12:25:17 +0100

    [ISS] feat: implement register endpoint; closes #27

--- ISS/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/controller/AuthController.java
+++ ISS/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/controller/AuthController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.request.LoginRequestDTO;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.request.RegisterRequestDTO;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.response.LoginResponseDTO;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.response.RegisterResponseDTO;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,6 +21,17 @@ public class AuthController {
                 "DRIVER", // or "REGISTERED_USER", "ADMINISTRATOR"
                 request.getEmail(),
                 "Login successful"
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO request) {
+        RegisterResponseDTO response = new RegisterResponseDTO(
+                request.getEmail(),
+                request.getUserType() != null ? request.getUserType() : "REGISTERED_USER",
+                "Registration successful"
         );
 
         return ResponseEntity.ok(response);
